677bf1e77be51594c5fb77847449238b641381dc
wuwang3817 <2421432410@qq.com>
2026-07-19 01:06:40 +0800

    拓展日志显示设置
    拓展日志显示设置

--- IOCPNet/IOCPTool.cs
+++ IOCPNet/IOCPTool.cs
@@ -12,7 +12,60 @@ namespace PENet
     public class IOCPTool
     {
         #region LOG
-        public static void ConsoleLog(string msg,IOCPColor color)
+        public static Action<string> LogFunc;
+        public static Action<IOCPColor,string> ColorLogFunc;
+        public static Action<string> WarnFunc;
+        public static Action<string> ErrorFunc;
+        public static void Log(string msg,params object[] args)
+        {
+            msg = string.Format(msg, args);
+            if (LogFunc != null)
+            {
+                LogFunc(msg);
+            }
+            else
+            {
+                ConsoleLog(msg, IOCPColor.None);
+            }
+        }
+        public static void ColorLog(IOCPColor color,string msg, params object[] args)
+        {
+            msg = string.Format(msg, args);
+            if (ColorLogFunc != null)
+            {
+                ColorLogFunc(color,msg);
+            }
+            else
+            {
+                ConsoleLog(msg, color);
+            }
+        }
+        public static void Warn(string msg, params object[] args)
+        {
+            msg = string.Format(msg, args);
+            if (WarnFunc != null)
+            {
+                WarnFunc(msg);
+            }
+            else
+            {
+                ConsoleLog(msg, IOCPColor.Yellow);
+            }
+        }
+        public static void Error(string msg, params object[] args)
+        {
+            msg = string.Format(msg, args);
+            if (ErrorFunc != null)
+            {
+                ErrorFunc(msg);
+            }
+            else
+            {
+                ConsoleLog(msg, IOCPColor.Red);
+            }
+        }
+        
+        private static void ConsoleLog(string msg,IOCPColor color)
         {
             int threadID=Thread.CurrentThread.ManagedThreadId;
             msg=string.Format("Thread:{0} {1}",threadID,msg);
