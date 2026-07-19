aef3f9811b9b0964f9dfb6c6a0e5eac313a76940
wuwang3817 <2421432410@qq.com>
2026-07-20 01:12:29 +0800

    IOCP连接会话Token
    IOCP连接会话Token

---
+++ IOCPNet/ICOPToken.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PENet
+{
+    //IOCP连接会话Token
+    public enum TokenState
+    {
+        None,
+        Connected,
+        Disconnected,
+    }
+    public class ICOPToken
+    {
+        private Socket skt;
+        public TokenState tokenState= TokenState.None;
+        private SocketAsyncEventArgs rcvSAEA;
+
+        public ICOPToken() 
+        {
+            rcvSAEA=new SocketAsyncEventArgs();
+            rcvSAEA.Completed+=new EventHandler<SocketAsyncEventArgs>(IO_Completed);
+        }
+
+        public void InitToken(Socket skt)
+        {
+            this.skt=skt;
+            tokenState= TokenState.Connected;
+            OnConnected();
+
+            StartAsyncRcv();
+        }
+
+        void StartAsyncRcv()
+        {
+            bool suspend=skt.ReceiveAsync(rcvSAEA);
+            if(!suspend)
+            {
+                ProcessRcv();
+            }
+        }
+
+        void ProcessRcv()
+        {
+
+        }
+        void IO_Completed(object sender, SocketAsyncEventArgs saea)
+        {
+            ProcessRcv();
+        }
+
+        void OnConnected()
+        {
+            IOCPTool.Log("Connect Success");
+        }
+    }
+}
